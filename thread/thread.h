79bdd7e6719ea1e766859f4416a19aef80431a4b
unknown <superking@.(none)>
2010-06-22 00:58:41 +0800

    Add Thread, template Mutex and template Locker classes.

---
+++ thread/thread.h
@@ -0,0 +1,43 @@
+#ifndef _THREAD_H_
+#define _THREAD_H_
+
+#include <Windows.h>
+#include <process.h>
+#include "../base/BaseTypes.h"
+
+namespace bittorrent
+{
+    namespace thread
+    {
+        typedef unsigned (*ThreadFunctionType) (void *);
+
+        class Thread : private NotCopyable
+        {
+        public:
+            Thread(ThreadFunctionType fun, void *funarg)
+                : fun_(fun),
+                  funarg_(funarg),
+                  handle_(0)
+            {
+                StartThread();
+            }
+
+            ~Thread()
+            {
+                CloseHandle((HANDLE)handle_);
+            }
+
+        private:
+            void StartThread()
+            {
+                handle_ = _beginthreadex(0, 0, fun_, funarg_, 0, 0);
+            }
+
+            ThreadFunctionType fun_;
+            void *funarg_;
+            unsigned handle_;
+        };
+    } // namespace thread
+} // namespace bittorrent
+
+#endif // _THREAD_H_
